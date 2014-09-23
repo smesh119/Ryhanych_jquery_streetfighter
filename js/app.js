3612df0254d0dfdc2fcb9d38fa6f7b22d22dcd60
smesh119 <smesh119@gmail.com>
2014-09-22 22:42:09 -0400

    Hadouken
    Added firing hadouken and sound.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+	function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
 	$(".ryu").mouseenter(function() {
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
@@ -8,12 +13,19 @@ $(document).ready(function() {
 		$(".ryu-still").show();
 	})
 	.mousedown(function(){
-		// play hadouken sound
+		playHadouken();
 		$(".ryu-ready").hide();
 		$(".ryu-throwing").show();
-		$(".hadouken").show();
-		//animate hadouken to the right of the screen
-	})
+		$(".hadouken").finish().show()
+		.animate(
+		{ "left":"300px"},
+		500,
+		function(){
+			$(this).hide();
+			$(this).css("left", "-212px");
+		}
+		);
+		})
 	.mouseup(function(){
 		$(".ryu-throwing").hide();
 		$(".ryu-ready").show();
